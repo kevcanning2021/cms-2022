1fe1d74f119bd0435fd1f4f85f3084e836de64cd
Kevin Canning <kevin@quazar.co.za>
2022-07-22 11:40:34 +0200

    Project setup.

---
+++ includes/sidebar.php
@@ -0,0 +1,43 @@
+<div class="col-md-4">
+<!-- Blog Search Well -->
+<div class="well">
+    <h4>Blog Search</h4>
+    <form action="search.php" method="post">
+    <div class="input-group">
+        <input name="search"  type="text" class="form-control">
+        <span class="input-group-btn">
+            <button name="submit" class="btn btn-default" type="submit">
+                <span class="glyphicon glyphicon-search"></span>
+        </button>
+        </span>
+    </div>
+    </form>
+    <!-- /.input-group -->
+</div>
+
+<!-- Blog Categories Well -->
+<div class="well">
+    <h4>Blog Categories</h4>
+    <div class="row">
+        <div class="col-lg-12">
+<?php
+                    $query = "SELECT * FROM categories LIMIT 3";
+                    $categories = mysqli_query($connection, $query);
+
+                    while($row = mysqli_fetch_assoc($categories)) {
+                        $title = $row['title'];
+                        
+                        echo "<ul class='list-unstyled'>";
+                        echo "<li><a href='#'>$title</a></li>";
+                        echo "</ul>";
+                    }
+                    ?>
+        </div>
+    </div>
+    <!-- /.row -->
+</div>
+
+<!-- Side Widget Well -->
+<?php include "widget.php"; ?>
+
+</div>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!-- Header -->
+<?php include "includes/header.php"; ?>
+<!-- Navigation -->
+<?php include "includes/navigation.php"; ?>
+<!-- Page Content -->
+<div class="container">
+
+    <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+            <?php
+                    $query = "SELECT * FROM posts";
+                    $posts = mysqli_query($connection, $query);
+
+                    while($row = mysqli_fetch_assoc($posts)) {
+                        $title = $row['title'];
+                        $author = $row['author'];
+                        $date = $row['date'];
+                        $image = $row['image'];
+                        $content = $row['content'];           
+            ?>
+
+            <h1 class="page-header">
+                Page Heading
+                <small>Secondary Text</small>
+            </h1>
+
+            <!-- First Blog Post -->
+            <h2>
+                <a href="#"><?php echo $title; ?></a>
+            </h2>
+            <p class="lead">
+                by <a href="index.php"><?php echo $author; ?></a>
+            </p>
+            <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $date; ?></p>
+            <hr>
+            <img class="img-responsive" src="images/<?php echo $image; ?>" alt="">
+            <hr>
+            <p>
+                <?php echo $content; ?>
+            </p>
+            <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+            <hr>
+            <?php } ?>           
+        </div>
+
+        <!-- Blog Sidebar Widgets Column -->
+        <?php include "includes/sidebar.php"; ?>
+    </div>
+
+    <hr>
+    <!-- Footer -->
+    <?php include "includes/footer.php"; ?>
