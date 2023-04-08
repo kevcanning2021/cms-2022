057881f33f1c34e1dab6f8626e4fe58a3930618c
Kevin Canning <kevin@quazar.co.za>
2023-04-08 17:08:25 +0200

    Security to delete users only if you are an admin.

--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -16,60 +16,64 @@
     </thead>
     <tbody>
         <?php
-            $query = "SELECT * FROM users";
-            $all_users_query = mysqli_query($connection, $query);
-        
-            while($row = mysqli_fetch_assoc($all_users_query)) { 
-                $id = $row['id']; 
-                $username = $row['username']; 
-                $firstname = $row['firstname']; 
-                $lastname = $row['lastname']; 
-                $email = $row['email']; 
-                $image = $row['image']; 
-                $role = $row['role'];       
+        $query = "SELECT * FROM users";
+        $all_users_query = mysqli_query($connection, $query);
 
-                echo "<tr>";
-                echo "<td>{$id}</td>";
-                echo "<td>{$username}</td>";
-                echo "<td>{$firstname}</td>";
-                echo "<td>{$lastname}</td>";
-                echo "<td>{$email}</td>";
-                echo "<td><img src='../images/{$image}' class='img-responsive' style='width:100px;'/></td>"; 
-                echo "<td>{$role}</td>";
-                echo "<td><a href='users.php?change_to_admin={$id}'>Admin</a></td>";
-                echo "<td><a href='users.php?change_to_subscriber={$id}'>Subscriber</a></td>";
-                echo "<td><a href='users.php?source=edit_user&u_id={$id}'>Edit</a></td>";
-                echo "<td><a href='users.php?delete={$id}'>Delete</a></td>";
-                echo "</tr>";
-            } ?>
+        while ($row = mysqli_fetch_assoc($all_users_query)) {
+            $id = $row['id'];
+            $username = $row['username'];
+            $firstname = $row['firstname'];
+            $lastname = $row['lastname'];
+            $email = $row['email'];
+            $image = $row['image'];
+            $role = $row['role'];
+
+            echo "<tr>";
+            echo "<td>{$id}</td>";
+            echo "<td>{$username}</td>";
+            echo "<td>{$firstname}</td>";
+            echo "<td>{$lastname}</td>";
+            echo "<td>{$email}</td>";
+            echo "<td><img src='../images/{$image}' class='img-responsive' style='width:100px;'/></td>";
+            echo "<td>{$role}</td>";
+            echo "<td><a href='users.php?change_to_admin={$id}'>Admin</a></td>";
+            echo "<td><a href='users.php?change_to_subscriber={$id}'>Subscriber</a></td>";
+            echo "<td><a href='users.php?source=edit_user&u_id={$id}'>Edit</a></td>";
+            echo "<td><a href='users.php?delete={$id}'>Delete</a></td>";
+            echo "</tr>";
+        } ?>
     </tbody>
 </table>
 
 <?php
-if(isset($_GET['change_to_admin'])) {
+if (isset($_GET['change_to_admin'])) {
     $id = $_GET['change_to_admin'];
-    
+
     $query = "UPDATE users SET role = 'admin' WHERE id = {$id}";
     $change_admin_query = mysqli_query($connection, $query);
-    
-    header("Location: users.php");
- }
 
-if(isset($_GET['change_to_subscriber'])) {
+    header("Location: users.php");
+}
+
+if (isset($_GET['change_to_subscriber'])) {
     $id = $_GET['change_to_subscriber'];
-    
+
     $query = "UPDATE users SET role = 'subscriber' WHERE id = {$id}";
     $change_subscriber_query = mysqli_query($connection, $query);
-    
-    header("Location: users.php");
- }
 
-    if(isset($_GET['delete'])) {
-       $id = $_GET['delete'];
-       
-       $query = "DELETE FROM users WHERE id = {$id}";
-       $delete_query = mysqli_query($connection, $query);
-       
-       header("Location: users.php");
+    header("Location: users.php");
+}
+
+if (isset($_GET['delete'])) {
+    if (isset($_SESSION['user_role'])) {
+        if ($_SESSION['user_role'] == 'admin') {
+            $id = $_GET['delete'];
+
+            $query = "DELETE FROM users WHERE id = {$id}";
+            $delete_query = mysqli_query($connection, $query);
+
+            header("Location: users.php");
+        }
     }
+}
 ?>
